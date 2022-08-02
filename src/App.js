e87a4e77f4463a11f0e3fa81ff6731c37268a11d
Shubhangi A. Patil <shubhangi.patil@tsilogistics.local>
2022-08-02 06:21:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment is happening here.
+          Deployment happens here.
         </p>
         <a
           className="App-link"
